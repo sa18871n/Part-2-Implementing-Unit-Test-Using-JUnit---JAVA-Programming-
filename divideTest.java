ba1cb59db0c7fcc92d3efcbe6fb5857ae73fd661
sa18871n <115948000+sa18871n@users.noreply.github.com>
2022-11-10 15:10:26 -0500

    Update divideTest.java

--- divideTest.java
+++ divideTest.java
@@ -7,11 +7,20 @@ import org.junit.Test;
 public class divideTest {
 
 	@Test
-	public void test() {
+	void negativetest() {
+		JunitTesting test=new JunitTesting();
+		Exception exception=assertThrows(ArithmeticException.class,() -> test.divide(1,0));
+		assertEquals("divide by 0",exception.getMessage());
+		
+		assertTrue(exception.getMessage().contains("zero"));
+		
+		
+	}
+	@Test
+	void positiveTest(){
 		JunitTesting test=new JunitTesting();
 		double output=test.divide(4,2);
 		assertEquals(2,output,0.001);
-		
 	}
 
 }
